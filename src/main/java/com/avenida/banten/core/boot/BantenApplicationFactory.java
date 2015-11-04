5f02cea9968397408cb0642c0ebfb2c417aef4fe
waabox <emi@avenida.com>
2015-11-04 19:35:30 -0300

    Add javadoc, still WIP

--- src/main/java/com/avenida/banten/core/boot/BantenApplicationFactory.java
+++ src/main/java/com/avenida/banten/core/boot/BantenApplicationFactory.java
@@ -2,15 +2,26 @@ package com.avenida.banten.core.boot;
 
 import org.springframework.boot.SpringApplication;
 
-/** TODO doc.me
+/** Factory for Banten Applications.
+ *
  * @author waabox (emi[at]avenida[dot]com)
  */
 public abstract class BantenApplicationFactory {
 
+  /** Retrieves the list of modules.
+   * @return the list of modules, never null.
+   */
   public abstract Class[] modules();
 
+  /** Retrieves the infrastructure modules.
+   * @return the list of modules, never null.
+   */
   public abstract Class[] with();
 
+  /** Creates the Spring application.
+   * @param mainClass the main class for lookup beans.
+   * @return the Spring Application, never null.
+   */
   public SpringApplication create(final Class mainClass) {
     BantenApplication application = new BantenApplication(modules());
     application.with(with());
--- src/main/java/com/avenida/banten/core/database/PlatformTuplizer.java
+++ src/main/java/com/avenida/banten/core/database/PlatformTuplizer.java
@@ -4,58 +4,79 @@ import java.util.List;
 
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.metamodel.binding.EntityBinding;
-import org.hibernate.tuple.Instantiator;
-import org.hibernate.tuple.PojoInstantiator;
-import org.hibernate.tuple.entity.EntityMetamodel;
-import org.hibernate.tuple.entity.PojoEntityTuplizer;
 
-import com.avenida.banten.core.ModuleServiceLocator;
-import com.avenida.banten.core.PersistenceUnit;
+import org.hibernate.tuple.*;
+import org.hibernate.tuple.entity.*;
 
-/**
+import com.avenida.banten.core.*;
+
+/** Platform Tuplizer that use the Factories declared within
+ * the PersistenceUnit.
+ *
  * @author waabox (emi[at]avenida[dot]com)
  */
 public class PlatformTuplizer extends PojoEntityTuplizer {
 
+  /** Creates a new instance of the Tuplizer.
+   * @param entityMetamodel the entity model.
+   * @param mappedEntity the mapped entity.
+   */
   public PlatformTuplizer(final EntityMetamodel entityMetamodel,
       final PersistentClass mappedEntity) {
     super(entityMetamodel, mappedEntity);
   }
 
+  /** Creates a new instance of the Tuplizer.
+   * @param entityMetamodel the entity model.
+   * @param mappedEntity the mapped entity.
+   */
   public PlatformTuplizer(final EntityMetamodel entityMetamodel,
       final EntityBinding mappedEntity) {
     super(entityMetamodel, mappedEntity);
   }
 
+  /** {@inheritDoc}.*/
   @Override
   protected Instantiator buildInstantiator(
       final PersistentClass persistentClass) {
     return new BantenInstantiator(persistentClass);
   }
 
+  /** Instantiator that serch within the PersistenceUnits the ones that
+   * has been declared a Factory and use it as Instantiator.
+   *
+   * @author waabox (emi[at]avenida[dot]com)
+   */
   public static class BantenInstantiator extends PojoInstantiator {
 
+    /** The serial version. */
     private static final long serialVersionUID = 1L;
 
-    private PersistentClass klass;
+    /** The Hibernate's persistent class, it's never null. */
+    private final PersistentClass persistentClass;
 
-    public BantenInstantiator(final PersistentClass persistentClass) {
-      super(persistentClass, null); // check the optimizer!
-      klass = persistentClass;
+    /** Creates a new instance of the Instantiator.
+     * @param aPersistentClass the Hibernate's persistent class.
+     */
+    public BantenInstantiator(final PersistentClass aPersistentClass) {
+      super(aPersistentClass, null); // check the optimizer!
+      persistentClass = aPersistentClass;
     }
 
+    /** {@inheritDoc}.*/
     @Override
     public Object instantiate() {
-      List<PersistenceUnit> lt = ModuleServiceLocator.getBean(
-          "persistenceUnitList");
-      for(PersistenceUnit pu : lt) {
-        if (pu.getPersistenceClass().getName().equals(klass.getClassName())) {
+      List<PersistenceUnit> persistenceUnits;
+      persistenceUnits = ModuleServiceLocator.getBean("persistenceUnitList");
+      // TODO [waabox] I do really need to cache this.
+      for(PersistenceUnit pu : persistenceUnits) {
+        if (pu.getPersistenceClass().getName().equals(
+            persistentClass.getClassName())) {
           return ModuleServiceLocator.getBean(pu.getFactory()).create();
         }
       }
       return super.instantiate();
     }
-
   }
 
 }
