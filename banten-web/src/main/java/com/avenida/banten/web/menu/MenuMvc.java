9f8b6037ebb702e19b2f93af61ae0cbb634b3255
waabox <emi@avenida.com>
2016-06-02 17:28:27 -0300

    remove the menu bean

--- banten-web/src/main/java/com/avenida/banten/web/menu/MenuMvc.java
+++ banten-web/src/main/java/com/avenida/banten/web/menu/MenuMvc.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import com.avenida.banten.shiro.ShiroConfigurationApi;
+
 import com.avenida.banten.web.freemarker.FreeMarkerViewResolver;
 import com.avenida.banten.web.menu.application.MenuController;
 
@@ -20,11 +21,10 @@ public class MenuMvc {
   /** Retrieves the {@link MenuController}.
    * @return the {@link MenuController}, never null.
    */
-  @Bean
-  public MenuController menuController(
-      final Menu menu,
+  @Bean public MenuController menuController(
+      final MenuConfigurationApi api,
       final MenuSecurityFilter filter) {
-    return new MenuController(menu, filter);
+    return new MenuController(api.get(), filter);
   }
 
   @Bean public SecuredUrlService urlService(final ShiroConfigurationApi api) {
@@ -33,10 +33,6 @@ public class MenuMvc {
 
   @Bean public RoleVoter roleVoter() {
     return new RoleVoter();
-  }
-
-  @Bean public Menu menu(final MenuConfigurationApi api) {
-    return api.get();
   }
 
   @Bean public MenuSecurityFilter filter(
